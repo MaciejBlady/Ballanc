eb30417572e1732f774e0930bd44700cf0021152
MaciejBlady <maciej.blady@gmail.com>
2017-01-16 20:48:02 +0100

    Beeper

---
+++ Assets/Scripts/Beeper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beeper : MonoBehaviour {
+
+    [SerializeField] private GameObject _target;
+    [SerializeField] private AudioClip _beep;
+
+    private float _distance = BEEP_FACTOR;
+
+    private const float BEEP_FACTOR = 0.4f;
+
+    void Start ()
+    {
+        Beep();
+	}
+	
+	void Update ()
+    {
+        _distance = Vector3.Distance(transform.position, _target.transform.position);
+	}
+
+    private void Beep()
+    {
+        AudioSource.PlayClipAtPoint(_beep, transform.position);
+        Invoke("Beep", BeepPeriod());
+    }
+
+    private float BeepPeriod()
+    {
+        return BEEP_FACTOR / _distance ;
+    }
+}
