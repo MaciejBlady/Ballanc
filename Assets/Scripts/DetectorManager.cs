a5d9080bcc436d6bf49517463d9f4dfe2abe0253
MaciejBlady <maciej.blady@gmail.com>
2017-01-17 22:30:06 +0100

    Warnings

---
+++ Assets/Scripts/DetectorManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorManager : MonoBehaviour
+{
+    public GameObject[] Detectors;
+    public AudioClip WarningSound;
+
+    private const float SCAN_TIME = 1.0f;
+
+	void Start ()
+    {
+        ScanAndWarn();
+    }
+
+    void ScanAndWarn()
+    {
+        float maxTilt = -1.0f;
+        GameObject maxTiltGO = null;
+        foreach (var detector in Detectors)
+        {
+            float diff = detector.GetComponent<TiltMeter>().NormalizedDifference;
+            detector.GetComponent<MeshRenderer>().material.color = Color.white;
+            if (diff > maxTilt)
+            {
+                maxTilt = diff;
+                maxTiltGO = detector;
+            }
+        }
+
+        maxTiltGO.GetComponent<MeshRenderer>().material.color = Color.red;
+        AudioSource.PlayClipAtPoint(WarningSound, maxTiltGO.transform.position);
+        Invoke("ScanAndWarn", SCAN_TIME - maxTilt);
+    }
+
+}
---
+++ Assets/Scripts/TiltMeter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltMeter : MonoBehaviour {
+
+    public GameObject Target;
+
+    private MeshRenderer _renderer;
+    private float _difference = 0.0f;
+
+    public float NormalizedDifference
+    {
+        get
+        {
+            return _difference;
+        }
+    }
+
+    void Start ()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+	
+	
+	void Update ()
+    {
+        float difference = transform.position.y - Target.transform.position.y;
+        Color color = Color.green;
+
+        _difference = Mathf.Clamp01(difference);
+
+        if (difference > 0.0f)
+        {
+            color = new Color(_difference, 0.0f, 0.0f);
+        }
+
+        //_renderer.material.color = color;       
+    }
+}
