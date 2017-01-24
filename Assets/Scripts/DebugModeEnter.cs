d145a765e3d36e166760c8823bfef7f71df4a461
Daniel Fernes <daniel.fernes@gmail.com>
2017-01-24 23:55:09 +0100

    a

---
+++ Assets/Scripts/DebugModeEnter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugModeEnter : MonoBehaviour
+{
+    private int _clickCount;
+
+    public void Click()
+    {
+        _clickCount++;
+        if (_clickCount > 10.0f)
+        {
+            SceneManager.LoadScene("test");
+        }
+    }
+}
